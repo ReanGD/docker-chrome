1c14e533b091e21f7aeaaf8029d942a26dd47d30
ReanGD <reangd@yandex.ru>
2017-03-03 02:13:17 +0300

    simple neuron

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hello")
+import tensorflow as tf
+
+
+def one_low_neuron_level(x_train, y_train, check_value):
+    # Model parameters
+    W = tf.Variable([.3], dtype=tf.float32)
+    b = tf.Variable([-.3], dtype=tf.float32)
+
+    # Model input and output
+    x = tf.placeholder(tf.float32)
+    linear_model = W * x + b
+    y = tf.placeholder(tf.float32)
+
+    # loss
+    loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+
+    # optimizer
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = optimizer.minimize(loss)
+
+    # training loop
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init) # reset values to wrong
+    for i in range(1000):
+      sess.run(train, {x: x_train, y: y_train})
+
+    # evaluate training accuracy
+    curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+    print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+    print(sess.run(linear_model, {x: check_value}))
+
+
+x_train = list(range(1, 7))
+y_train = [-it * 2 + 7 for it in x_train]
+check_value = 100
+one_low_neuron_level(x_train, y_train, check_value)
